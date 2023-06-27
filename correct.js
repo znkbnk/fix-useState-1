60837d894b9f0e2aaa7b25fa99ef37e44a735640
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-27 21:26:51 +0100

    Create correct.js

---
+++ correct.js
@@ -0,0 +1,21 @@
+//correct version
+
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
+
